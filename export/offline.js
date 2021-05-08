3d82c785d0dcc342a30f068af10ccec10aab6e65
fishkindahot <betafishu@gmail.com>
2021-05-08 20:37:44 +0200

    App Build

---
+++ export/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1620319827,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/mask-sheet0.png",
+		"images/daysbar-sheet0.png",
+		"images/hoursbar-sheet0.png",
+		"images/minutesbar-sheet0.png",
+		"images/secondsbar-sheet0.png",
+		"images/protectionlayer-sheet0.png",
+		"images/background-sheet0.png",
+		"images/truebackground-sheet0.png",
+		"images/camera-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"generator_config.txt",
+		"nunito.regular-demo.html",
+		"nunito.regular-webfont.woff",
+		"nunito.regular-webfont.woff2",
+		"stylesheet.css"
+	]
+}
